64691dd99237480cc33f40d8baf8d04f74b33a22
LucasBerra <60557260+LucasBerra@users.noreply.github.com>
2021-07-22 12:06:03 -0300

    Add files via upload
    Added main page to repository.

---
+++ js/main.js
@@ -0,0 +1,53 @@
+
+let loading = document.querySelector("#loading");
+
+
+
+const loadingToItems = (items) => {
+  loading.textContent = `${items} items.`;
+}
+
+const fetchAPI = (pageCategory) => {
+  fetch("https://fakestoreapi.com/products")
+    .then(res => res.json())
+    .then(json => showData(json, pageCategory))
+    .catch(err => console.log("Something went wrong:", err))
+}
+
+const showData = (jsonObj, pageCategory) => {
+  
+  targetContainer.innerHTML = "";
+  let totalItems = 0;
+
+  jsonObj.forEach(jsonItem => {
+    let { id, title, price, description, category, image } = jsonItem;
+
+    let listItem = document.createElement("div");
+    listItem.setAttribute("class", "listItem");
+    if (pageCategory === category) {
+      listItem.innerHTML = `
+      <div>
+        <h4>${title}</h4>
+        <p>${description}</p>
+        <h5>Price: $${price}</h5>
+      </div>
+    
+      <img src="${image}">
+      `;
+
+      targetContainer.appendChild(listItem);
+      totalItems += 1;
+    }
+  });
+
+  loadingToItems(totalItems);
+  console.log(`There are ${totalItems} items in the list`);
+}
+
+let targetContainer = document.querySelector("#targetContainer");
+
+let pageCategory = document.querySelector("#page-value").textContent;
+window.addEventListener("load", fetchAPI(pageCategory.toLowerCase()));
+
+let fetchBtn = document.querySelector("#fetchBtn");
+fetchBtn.addEventListener("click", fetchAPI("jewelery"))
