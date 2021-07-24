4c41a4f26ecc1ad4016b6d9526a64f782f1b9e61
LucasBerra <60557260+LucasBerra@users.noreply.github.com>
2021-07-23 22:45:03 -0300

    Add files via upload
    - Updated list layout.
    - Added item browsing.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
-let loading = document.querySelector("#loading");
+/* MAIN LIST FETCHING */
 
+let loading = document.querySelector("#loading");
 
 const loadingToItems = (items) => {
   loading.textContent = `${items} items.`;
@@ -8,6 +9,7 @@ const loadingToItems = (items) => {
 }
 
 const fetchAPI = (pageCategory) => {
+  
   fetch("https://fakestoreapi.com/products")
     .then(res => res.json())
     .then(json => showData(json, pageCategory))
@@ -24,12 +26,15 @@ const showData = (jsonObj, pageCategory) => {
 
     let listItem = document.createElement("div");
     listItem.setAttribute("class", "listItem");
+    listItem.setAttribute("id", `${id}`);
+    listItem.setAttribute("onclick", `displayItem(${id})`);
+    
     if (pageCategory === category) {
       listItem.innerHTML = `
       <div>
         <h4>${title}</h4>
         <p>${description}</p>
-        <h5>Price: $${price}</h5>
+        <h5>Price: US$${price}</h5>
       </div>
       <div id="listItem--img">
         <img src="${image}">
@@ -37,6 +42,8 @@ const showData = (jsonObj, pageCategory) => {
       `;
 
       targetContainer.appendChild(listItem);
+      document.querySelector("#title").removeAttribute("class", "hidden");
+      document.querySelector("#list-btn").style.display = "none";
       totalItems += 1;
     }
   });
@@ -48,3 +55,46 @@ let targetContainer = document.querySelector("#targetContainer");
 
 let pageCategory = document.querySelector("#page-value").textContent;
 window.addEventListener("load", fetchAPI(pageCategory.toLowerCase()));
+
+
+
+const displayItem = (itemId) => {
+
+  fetch("https://fakestoreapi.com/products")
+    .then(res => res.json())
+    .then(json => {
+      json.forEach(jsonItem => {
+        let { id, title, price, description, category, image } = jsonItem;
+
+        if (itemId === id) {
+          let itemBody = document.createElement("div");
+          itemBody.setAttribute("class", "itemBody");
+
+          itemBody.innerHTML = `
+          <img src="${image}">
+
+          <div>
+            <h4>${title}</h4>
+            <h5>-${category}-</h5>
+            <hr>
+            <h6>Description:</h6>
+            <p>${description}</p>
+            <hr>
+            <h5>Price: US$${price}</h5>
+          </div>
+          `;
+
+          targetContainer.innerHTML = "";
+          targetContainer.appendChild(itemBody);
+
+          document.querySelector("#title").className += "hidden";
+          document.querySelector("#list-btn").style.display = "initial";
+        }
+      })
+    })
+    .catch(err => console.log("Something went wrong:", err))
+}
+
+const backToList = () => {
+  fetchAPI(pageCategory.toLowerCase());
+}
